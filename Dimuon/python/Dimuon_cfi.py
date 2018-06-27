d43e8996cf8adf761782a26e808b2519b814b15e
Prakash Thapa <pthapa1g@gmail.com>
2018-06-27 14:15:57 -0500

    check commiting changes to Dimuon_cfi.py

--- Dimuon/python/Dimuon_cfi.py
+++ Dimuon/python/Dimuon_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 dimuon = cms.EDAnalyzer('Dimuon',
                       genPartsTag=cms.InputTag("genParticles"),
                       genInfoProduct=cms.InputTag("generator"),
+                      genEventInfoProduct=cms.InputTag("generator"),
                       decayParticlePID=cms.int32(13),
                       debug=cms.int32(0),
 )
